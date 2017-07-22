02c5fbd2259bbe01f9af56256443c6cfd7879151
Md Taufique Hussain <taufique71@gmail.com>
2017-07-22 23:30:53 +0600

    Adds skeleton of main file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import json
 import graph
+import jobmanager
 
 def main():
     config = json.loads( open("config.json").read() )
     job_dependency_graph = graph.Graph(config)
-    print(job_dependency_graph.get_vertices());
-    print(job_dependency_graph.get_edges());
+    out_file=open("./test_scripts/out.txt", "w")
+    manager = jobmanager.Manager(job_dependency_graph, config, stdin=None, stdout=out_file, stderr=None)
+    manager.start_jobs()
+    # print(job_dependency_graph.outdegree);
 
 if __name__ == "__main__":
     main()
