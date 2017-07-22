84bb1846c06c70a425ca3507aacdf0b3f3632f75
Md Taufique Hussain <taufique71@gmail.com>
2017-07-22 12:33:24 +0600

    Completes body of four helper functions in graph module

--- graph.py
+++ graph.py
@@ -4,16 +4,19 @@ class Graph:
     config = {}
     outdegree = {}
 
+    def update_outdegree(self):
+        for v in self.vertices:
+            self.outdegree[v] = 0
+        for e in self.edges:
+            self.outdegree[e[0]] = self.outdegree[e[0]]+1
+
     def __init__(self, config):
         self.config = config
         self.vertices = list(config["jobs"].keys())
         for key, value in config["dependencies"].items():
             for val in value:
                 self.edges.append( (key, val) )
-        for v in self.vertices:
-            self.outdegree[v] = 0
-        for e in self.edges:
-            self.outdegree[e[0]] = self.outdegree[e[0]]+1
+        self.update_outdegree()
 
     def get_vertices(self):
         return self.vertices
@@ -22,11 +25,27 @@ class Graph:
         return self.edges
 
     def get_zero_outdegree_vertices(self):
-        return []
+        zero_outdegree_vertices = []
+        for key, value in self.outdegree.items():
+            if value == 0:
+                zero_outdegree_vertices.append(key)
+        return zero_outdegree_vertices
 
     def remove_edge(self, edge):
-        return
+        try:
+            self.edges.remove( edge )
+            self.update_outdegree()
+            return
+        except:
+            return
 
-    def remove_vertex(self, edge):
-        return
+    def remove_vertex(self, vertex):
+        try:
+            self.vertices.remove(vertex)
+            for e in self.edges:
+                if e[0] == vertex or e[1] == vertex:
+                    self.remove_edge(e)
+            return
+        except:
+            return
 
