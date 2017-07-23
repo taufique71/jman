48e68fa28d0ecb424ba433337ab22b626bcad96a
Md. Taufique Hussain <taufique@muktosoft.com>
2017-07-23 19:38:58 +0600

    Refactors job module to add stopping functionality

--- job.py
+++ job.py
@@ -1,27 +1,77 @@
-from threading import Thread
+import threading
 import subprocess
+import psutil
+import datetime
+import time
+import signal
 
-class Job(Thread):
-    def __init__(self, job_key, job_details, parent=None, stdout=None):
+class Job(threading.Thread):
+    def __init__(self, job_key, job_details, parent, stdout=None):
         self.job_key = job_key
         self.job_details = job_details
         self.parent = parent
         self.stdout = stdout
-        # if self.stdout == None:
-            # self.stdout = open(self.job_details["path"] + job_key + ".txt", "w")
+        self.stop_event = threading.Event()
         super(Job, self).__init__()
-        return
+
+    def stop(self):
+        self.stop_event.set()
 
     def monitor(self, proc):
-        outs, errs = proc.communicate()
-        if errs:
-            return "failed"
+        parent = psutil.Process(proc.pid)
+        parent_status = parent.status()
+        children = parent.children(recursive=True)
+        if parent_status != psutil.STATUS_ZOMBIE:
+            if parent_status != psutil.STATUS_DEAD:
+                return "processing"
+            else:
+                return "failed"
         else:
-            return "success"
-        return
+            if len(children) == 0:
+                outs, errs = proc.communicate()
+                if errs:
+                    return "failed"
+                else:
+                    return "success"
+            else:
+                for c in children:
+                    if c.status() == psutil.STATUS_DEAD:
+                        return "failed"
+                return "processing"
+
+    def kill_processes(self, proc=None):
+        if proc == None:
+            return
+        else:
+            parent = psutil.Process(proc.pid)
+            children = parent.children(recursive=True)
+            for c in children:
+                c.send_signal(signam.SIGTERM)
+            parent.send_signal(signal.SIGTERM)
 
     def run(self):
-        script_to_run = self.job_details["path"] + self.job_details["script"]
-        proc = subprocess.Popen(script_to_run, shell=True, stdout=self.stdout)
-        job_status = self.monitor(proc)
-        self.parent and self.parent.on_job_finish(self.job_key, job_status)
+        job_status = "processing"
+        proc = None
+
+        if self.job_details["time"]:
+            today = datetime.date.today()
+            target_time = datetime.datetime.strptime(self.job_details["time"], "%H:%M").time()
+            target_datetime = datetime.datetime.combine(today, target_time)
+            now = datetime.datetime.now()
+            while now < target_datetime and self.stop_event.is_set() == False:
+                time.sleep(1)
+                now = datetime.datetime.now()
+
+        if self.stop_event.is_set() == False:
+            script_to_run = self.job_details["path"] + self.job_details["script"]
+            proc = subprocess.Popen(script_to_run, shell=True, stdout=self.stdout)
+
+        while self.stop_event.is_set() == False:
+            job_status = self.monitor(proc)
+            if job_status != "processing":
+                break
+
+        if self.stop_event.is_set() == False:
+            self.parent.on_job_finish(self.job_key, job_status)
+        else:
+            self.kill_processes(proc)
